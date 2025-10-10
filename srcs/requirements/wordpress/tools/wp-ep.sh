dc2880be79a0ac1b37be670c70f66e9ad5656d08
Pachiderms <79471497+Pachiderms@users.noreply.github.com>
2025-10-10 11:34:59 +0200

    Update wp-ep.sh

--- srcs/requirements/wordpress/tools/wp-ep.sh
+++ srcs/requirements/wordpress/tools/wp-ep.sh
@@ -1,60 +1,43 @@
 #!/bin/bash
 
-# db_name=wordpress
-# db_user=mysql_user
-# db_pwd=pass_42
-# db_root_pwd=root_pass_42
-
+db_name=wordpress
+db_user=mysql
+db_pwd=pass_42
 sleep 10
 
-# create directory to use in nginx container later and also to setup the wordpress conf
-# mkdir /var/www/
-# mkdir /var/www/html
-
+mkdir -p /var/www/html
 cd /var/www/html
 
-
-rm -rf *
-
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
-chmod +x wp-cli.phar 
-
+# install wp-cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-
+# download wordpress
 wp core download --allow-root
 
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# configure wp-config
+mv wp-config-sample.php wp-config.php
 
-mv /wp-config.php /var/www/html/wp-config.php
+sed -i "s/database_name_here/$db_name/" wp-config.php
+sed -i "s/username_here/$db_user/" wp-config.php
+sed -i "s/password_here/$db_pwd/" wp-config.php
+sed -i "s/localhost/mariadb/" wp-config.php
 
+# install wordpress
+wp core install --allow-root \
+  --url="https://localhost:8443" \
+  --title="My Wordpress Site" \
+  --admin_user="pachiderms" \
+  --admin_password="thepwd" \
+  --admin_email="mail@example.com"
 
-sed -i -r "s/database_name_here/$db_name/1"   /var/www/html/wp-config.php
-sed -i -r "s/username_here/$db_user/1"  /var/www/html/wp-config.php
-sed -i -r "s/password_here/$db_pwd/1"    /var/www/html/wp-config.php
+wp user create user user@example.com --role=author --user_pass=userpass --allow-root
 
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-
-
-
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-
+# plugins
 wp theme install astra --activate --allow-root
-
-
 wp plugin install redis-cache --activate --allow-root
-
 wp plugin update --all --allow-root
 
-
- 
-sed -i 's/listen = \/run\/php\/php8.4-fpm.sock/listen = 9000/g' /etc/php/8.4/fpm/pool.d/www.conf
-
-#mkdir /run/php
-
-wp redis enable --allow-root
-
-/usr/sbin/php-fpm8.4 -F
+mkdir -p /run/php
+php-fpm8.4 -F
